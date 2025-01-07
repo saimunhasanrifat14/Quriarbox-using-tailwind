4a5452eafe7e37daaeecf83c39692c6849edf5b3
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-01-07 22:56:11 +0600

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  fontFamily :{
+    raleway :  "var(--raleway)",
+  },
+  container: {
+    padding: {
+      DEFAULT: '2rem',
+
+    },
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
